88ffc75fc6fc1048528807365a363e821ddb4d5e
Antonino <drakon9927@libero.it>
2024-05-01 14:55:59 +0200

    m2 fixes

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 let griglia = document.getElementById(`grid`);
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i <= 100; i++) {
+    let elemento = creaQuadrato();
+
+    griglia.append(elemento);
+
+
     
+}
+
+function creaQuadrato(contenuto) {
+    // Creo un quadrato
     let square = document.createElement("div");
     square.classList.add("square");
-    square.innerText = i;
-    griglia.append(square);
-    
+    square.innerText = contenuto;
+   
+
+    // Aggiungo un event listener 
+
+    square.addEventListener("click", function(){
+        console.log("cliccato");
+    })
+
+    return square;
 }
